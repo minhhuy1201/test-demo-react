d3712c238460f89926d3cf334c1b960a378e3e48
huykirito1201 <huyyhuqqho@gmail.com>
2024-02-26 08:02:18 +0700

    add redux, loading bar and more

--- src/components/Home/HomePage.jsx
+++ src/components/Home/HomePage.jsx
@@ -1,17 +1,24 @@
-import videoHomePage from '../../assets/video-homepage.mp4'
-
 import './HomePage.scss'
+import videoHomePage from '../../assets/video-homepage.mp4'
+import { useSelector } from 'react-redux'
 
 const HomePage = props => {
+  const isAuthenticated = useSelector(state => state.user.isAuthenticated)
+  const account = useSelector(state => state.user.account)
+
   return (
     <div className='homepage-container'>
       <video autoPlay muted loop>
         <source src={videoHomePage} type='video/mp4' />
       </video>
 
-      <div className="home-page-content">
+      <div className='home-page-content'>
         <div className='title-1'>Lorem ipsum dolor sit amet.</div>
-        <div className='title-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia corrupti, eligendi vel corporis molestiae dolores aspernatur aliquam molestias quibusdam laborum!</div>
+        <div className='title-2'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
+          corrupti, eligendi vel corporis molestiae dolores aspernatur aliquam
+          molestias quibusdam laborum!
+        </div>
         <div>
           <button className='title-3'>Get start. It's free</button>
         </div>
--- src/utils/axiosConfig.js
+++ src/utils/axiosConfig.js
@@ -1,16 +1,24 @@
 import axios from 'axios'
+import NProgress from 'nprogress'
 
 const instance = axios.create({
   baseURL: 'http://localhost:8081/'
 })
 
+NProgress.configure({
+  showSpinner: false,
+  trickleSpeed: 150
+})
+
 // Add a request interceptor
 instance.interceptors.request.use(
   function (config) {
+    NProgress.start() // hien thanh progess khi nguoi dung request
     // Do something before request is sent
     return config
   },
   function (error) {
+    NProgress.start() // hien thanh progess khi nguoi dung request
     // Do something with request error
     return Promise.reject(error)
   }
@@ -19,11 +27,13 @@ instance.interceptors.request.use(
 // Add a response interceptor
 instance.interceptors.response.use(
   function (response) {
+    NProgress.done() // huy thanh progess khi tra ve ket qua
     // Any status code that lie within the range of 2xx cause this function to trigger
     // Do something with response data
     return response && response.data ? response.data : response
   },
   function (error) {
+    NProgress.done() // huy thanh progess khi tra ve ket qua
     // Any status codes that falls outside the range of 2xx cause this function to trigger
     // Do something with response error
     return error && error.response && error.response.data
